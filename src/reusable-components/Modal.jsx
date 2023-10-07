6b30fe99d1f1c4c5787a2010acd828ff19e307e7
Bekzod <bekzodisakov18@gmail.com>
2023-10-07 10:54:49 +0500

    modal trap focus

--- src/reusable-components/Modal.jsx
+++ src/reusable-components/Modal.jsx
@@ -5,16 +5,49 @@ import { motion } from "framer-motion";
 import { Icon } from "@reusable-components/Icon";
 
 export const Modal = ({ children, handleOnClose }) => {
+  const modalRef = React.useRef(null);
+
   React.useEffect(() => {
     document.body.style.overflow = "hidden";
 
+    const modalElement = modalRef.current;
+    const focusableElements = modalElement.querySelectorAll(
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"]'
+    );
+
+    const firstElement = focusableElements[0];
+    const lastElement = focusableElements[focusableElements.length - 1];
+    function handleEscapeKeyPress(e) {
+      if (e.key === "Escape") {
+        handleOnClose();
+      }
+    }
+
+    function handleTabKeyPress(e) {
+      if (e.shiftKey && document.activeElement === firstElement) {
+        e.preventDefault();
+        lastElement.focus();
+      } else if (!e.shiftKey && document.activeElement === lastElement) {
+        e.preventDefault();
+        firstElement.focus();
+      }
+    }
+
+    modalElement.focus();
+    modalElement.addEventListener("keydown", handleEscapeKeyPress);
+    modalElement.addEventListener("keydown", handleTabKeyPress);
+
     return () => {
       document.body.style.overflow = "unset";
+      modalElement.removeEventListener("keydown", handleEscapeKeyPress);
+      modalElement.removeEventListener("keydown", handleTabKeyPress);
     };
-  }, []);
+  }, [handleOnClose]);
 
   const ModalContent = () => (
     <motion.div
+      ref={modalRef}
+      tabIndex='-1'
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
@@ -27,13 +60,15 @@ export const Modal = ({ children, handleOnClose }) => {
         className={`bg-white rounded-md shadow-xl relative max-h-[95vh] overflow-auto`}
         onClick={(e) => e.stopPropagation()}
       >
-        <button
-          onClick={handleOnClose}
-          className='absolute top-2 right-2 bg-gray-200 p-1.5 rounded-sm'
-        >
-          <Icon name='x' />
-        </button>
-        <div>{children}</div>
+        <div>
+          <button
+            onClick={handleOnClose}
+            className='absolute top-2 right-2 bg-gray-200 p-1.5 rounded-sm'
+          >
+            <Icon name='x' />
+          </button>
+          <div>{children}</div>
+        </div>
       </motion.div>
     </motion.div>
   );
