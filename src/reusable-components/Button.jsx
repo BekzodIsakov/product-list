316916d27c1736c5563c93d0c2f45fad51874cfc
Bekzod <bekzodisakov18@gmail.com>
2023-10-08 12:11:00 +0500

    remove margin-top

--- src/reusable-components/Button.jsx
+++ src/reusable-components/Button.jsx
@@ -4,7 +4,7 @@ export const Button = ({ children, className = "", ...rest }) => {
   return (
     <button
       {...rest}
-      className={`inline-block w-20 p-1 rounded mt-2 border bg-indigo-500 text-slate-100 hover:bg-indigo-400 duration-75 font-light ${className}`}
+      className={`inline-block w-20 p-1 rounded border bg-indigo-500 text-slate-100 hover:bg-indigo-400 duration-75 font-light ${className}`}
     >
       {children}
     </button>
