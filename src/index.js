b1592d36d557880d1c6dfec519ef7f7421c3c2d0
Bekzod <bekzodisakov18@gmail.com>
2023-10-09 10:10:14 +0500

    rename ContextProvider to ProductContextProvider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ContextProvider } from "@context";
+import { ProductContextProvider } from "@context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContextProvider>
+    <ProductContextProvider>
       <App />
-    </ContextProvider>
+    </ProductContextProvider>
   </React.StrictMode>
 );
 
